0061ca24071ac7baa7ccf689b7498dd21120b067
runlingli <lirunlingemily@gmail.com>
2025-05-29 16:18:26 -0700

    style: add more hover style to the buttons, add function to alert after user giving up

--- frontend/src/SearchBar.svelte
+++ frontend/src/SearchBar.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import searchIcon from './assets/search.svg';
   let searchQuery = "";
+
+  //if the text input is not empty, the search button will be active (change to blue)
+  $: hasContent = searchQuery.trim().length > 0;
 </script>
 
 <div class="search-container">
@@ -10,7 +13,7 @@
     placeholder="Search for the game you want to guess..."
     class="search-input"
   />
-  <button class="search-button" aria-label="Search">
+  <button class="search-button" class:active={hasContent}>
     <img src={searchIcon} alt="Search" />
   </button>
 </div>
@@ -32,8 +35,9 @@
     font-weight: 600;
     color: #000000;
     background-color: white;
-    border-radius: 10px;
-    border: 1.2px solid #CCCCCC;
+    box-shadow: -4px 4px 4px 0px rgba(0, 0, 0, 0.15);
+    border-radius: 15px;
+    border: none;
     outline: none;
   }
 
@@ -49,14 +53,24 @@
     align-items: center;
     justify-content: center;
     background-color: #B3B5BD;
+    box-shadow: -4px 4px 4px 0px rgba(0, 0, 0, 0.15);
     border-radius: 16px;
     padding: 0;
     border: none;
+    cursor: pointer;
+    transition: background-color 0.3s ease;
+  }
+
+  .search-button.active {
+    background-color: #B9DBF3;
+  }
+  
+  .search-button:hover {
+    opacity: 0.9;
   }
 
   .search-button img {
     width: 24px;
     height: 24px;
   }
-
 </style>
